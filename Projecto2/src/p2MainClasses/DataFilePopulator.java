52ae4407649f9d751f2fbb0a6dd818e7a6103e71
EggieMetal <edgardo.muniz@upr.edu>
2016-10-07 13:29:09 -0400

    empezamos a movernos con las instrucciones. me quede aqui: "When the attributes are set,"

--- Projecto2/src/p2MainClasses/DataFilePopulator.java
+++ Projecto2/src/p2MainClasses/DataFilePopulator.java
@@ -3,6 +3,7 @@ package p2MainClasses;
 import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.util.Scanner;
 
 /**
  * This class initiates the execution of the application that 
@@ -14,17 +15,41 @@ import java.io.RandomAccessFile;
  *
  */
 public class DataFilePopulator {
-	
-	//voy a necesitar scanner
+
+	public void doNothing(){
+
+		String[] TYPES = {"byte", "char", "short", "int", "long", "float", "double",
+				"boolean", "date"};
+
+		//voy a necesitar scanner
+		Scanner sc = new Scanner(System.in);
+		boolean done = false;
+		while(!done)
+		{
+			System.out.println("Next Attribute Name:");
+			String attributeName = sc.nextLine();
+			System.out.println("Attribute Data Type:");
+			String attributeType = sc.next();
+			for( String types : TYPES)
+				if(!attributeType.equalsIgnoreCase(types))
+				{
+					System.out.println("not acceptable data type. reenter.");
+				}
+			System.out.println("want to add more attributes? 0= yes, 1 =no");
+			int more = sc.nextInt();
+			if(more == 1)
+				done=true;
+		}
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		RandomAccessFile file;
-		
+
 		String name = "edgardo", surname = "eggie";
 		int age = 21;
-		
+
 		try{
 			file = new RandomAccessFile(new File("inputData", "Data"), "rw");
 			long FileSize = file.length();
@@ -34,31 +59,31 @@ public class DataFilePopulator {
 			{
 				file.writeByte(20);
 			}
-			
+
 			file.writeUTF(surname);
 			for(int i=0; i<20-surname.length(); i++)
 			{
 				file.writeByte(20);
 			}
-			
+
 			file.writeInt(age);
-			
+
 			file.close();
 		}
 		catch(IOException e)
 		{
 			e.getStackTrace();
 		}
-		
-		
+
+
 		//----------------------------------------------------------------------
-		
+
 		name = null;
 		surname = null;
 		age = 0;
-		
+
 		try{
-			
+
 			file = new RandomAccessFile(new File("inputData", "Data"), "rw");
 			long FileSize = file.length();
 			file.seek(0);
@@ -70,38 +95,38 @@ public class DataFilePopulator {
 				{
 					file.readByte();
 				}
-				
+
 				surname = file.readUTF();
 				for(int i =0; i<20-surname.length(); i++)
 				{
 					file.readByte();
 				}
-				
+
 				age = file.readInt();
 				System.out.println(name +" "+ surname +" "+ age);
 			}
-			
+
 			file.close();
-			
+
 		} catch(IOException e)
 		{
 			e.getStackTrace();
 		}
 
 		//----------------------------------------------------------
-		
+
 		//create a string to search for
-		
+
 		//add an if statement at end of inside for loop
 		//that asks if the string searched = a 'name' in the file.
-		
+
 		//print the data you want to print after that string you have found.
-		
-		
+
+
 		//---------------------------------------------------------------
-		
+
 		//seek(RECORD+j+44)
-		
-		
+
+
 	}
 }
