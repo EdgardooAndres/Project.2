977e8a4317f17b6c3731aa05fcc1698299eccd9c
EggieMetal <edgardo.muniz@upr.edu>
2016-10-06 21:48:32 -0400

    added comments for searching, falta updating values.  Ademas añadi un comentario diciendo que voy a necesitar scanner.

--- Projecto2/src/p2MainClasses/DataFilePopulator.java
+++ Projecto2/src/p2MainClasses/DataFilePopulator.java
@@ -14,6 +14,8 @@ import java.io.RandomAccessFile;
  *
  */
 public class DataFilePopulator {
+	
+	//voy a necesitar scanner
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -86,5 +88,20 @@ public class DataFilePopulator {
 			e.getStackTrace();
 		}
 
+		//----------------------------------------------------------
+		
+		//create a string to search for
+		
+		//add an if statement at end of inside for loop
+		//that asks if the string searched = a 'name' in the file.
+		
+		//print the data you want to print after that string you have found.
+		
+		
+		//---------------------------------------------------------------
+		
+		//seek(RECORD+j+44)
+		
+		
 	}
 }
